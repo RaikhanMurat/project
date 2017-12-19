21ad6e5056e96e34aa5042d09c4c6b1c30a96145
RaikhanMurat <34675272+RaikhanMurat@users.noreply.github.com>
2017-12-19 14:32:27 +0600

    1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string t;
+                double p, s, a, p1;
+                Console.Write("Введите периметр = ");
+                t = Console.ReadLine();
+                p1 = double.Parse(t);
+                p = p1 / 2;
+                a = p1 / 3;
+                s = Math.Sqrt(p*Math.Pow(p-a,3));
+                Console.WriteLine("Сторона\tПлощадь");
+                Console.Write("{0:0.00}",a);
+                Console.WriteLine("\t{0:0.00}",s);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown:{0}",e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
